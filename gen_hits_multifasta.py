c6918c022a12298ec9def86fe9020ec56ee70211
jflucier <3505568+jflucier@users.noreply.github.com>
2024-01-18 13:41:36 -0500

    variable offset

--- gen_hits_multifasta.py
+++ gen_hits_multifasta.py
@@ -11,7 +11,7 @@ GB_EXT = ".gb"
 # checking for genes with location +/- OVERLAP_MARGIN in nt
 TARGET_OFFSET = 500
 
-def gen_multi_fasta(in_f, genome_path, out):
+def gen_multi_fasta(in_f, genome_path, out, offset=TARGET_OFFSET):
     res = pd.read_csv(in_f, sep='\t', index_col="accession.1")
     x = res.groupby('accession.1')
 
@@ -31,7 +31,7 @@ def gen_multi_fasta(in_f, genome_path, out):
             else:
                 print(f"Genbank found: {gb_path}")
                 genome = SeqIO.read(gb_path, "genbank")
-                target_seq = get_sequence(genome, start, end)
+                target_seq = get_sequence(genome, start, end, offset)
                 all_seqs.append({
                     'seq': target_seq,
                     'acc': a
@@ -51,19 +51,19 @@ def output_fa(out, all_seqs):
             c = c + 1
 
 
-def get_sequence(genome, start, end):
+def get_sequence(genome, start, end, offset):
     # Find all features that overlap the range
     seq_start_idx = 0
-    if start - TARGET_OFFSET < 0:
+    if start - offset < 0:
         seq_start_idx = 0
     else:
-        seq_start_idx = start - TARGET_OFFSET
+        seq_start_idx = start - offset
 
     seq_end_idx = len(genome.seq)
-    if end + TARGET_OFFSET > seq_end_idx:
+    if end + offset > seq_end_idx:
         seq_end_idx = len(genome.seq)
     else:
-        seq_end_idx = end + TARGET_OFFSET
+        seq_end_idx = end + offset
 
     return genome.seq[seq_start_idx:seq_end_idx]
 
