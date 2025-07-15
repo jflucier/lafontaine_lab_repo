270dd8236946fa7d4f7e49dab6ce5d22adf8ca18
jflucier <3505568+jflucier@users.noreply.github.com>
2025-07-15 12:42:20 -0400

    added gz gb file support

--- find_riboswitch_sequence.py
+++ find_riboswitch_sequence.py
@@ -61,9 +61,12 @@ def get_sequences(in_f, outpath, genome_base):
         if not os.path.exists(fa_path):
             print(f"Fasta not found. Falling back to toplevel file")
             fa_pattern = re.compile(".*.toplevel.fa.gz$")
-            for f in os.listdir(base_fa_path):
-                if fa_pattern.match(f):
-                    fa_path = os.path.join(base_fa_path, f)
+            for root, dirs, files in os.walk(base_fa_path):
+                for f in files:
+                    if fa_pattern.match(f):
+                        fa_path = os.path.join(root, f)
+                        break  # Break out of the inner loop (files)
+                if fa_path:  # If fa_path was found, break out of the outer loop (os.walk)
                     break
 
         if not os.path.exists(fa_path):
